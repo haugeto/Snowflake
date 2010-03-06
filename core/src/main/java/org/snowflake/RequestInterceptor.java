4ccc3919915fa5395d25a105e421ef44884d41cd
Anders Haugeto <anders.haugeto@gmail.com>
2010-03-06 14:13:38 +0100

    Javadoc

--- core/src/main/java/org/snowflake/RequestInterceptor.java
+++ core/src/main/java/org/snowflake/RequestInterceptor.java
@@ -1,5 +1,11 @@
 package org.snowflake;
 
+/**
+ * A request "filter" that is given the chance to pre and post process a request to the application.
+ * 
+ * @see WebApp#addRequestInterceptor(RequestInterceptor)
+ * @author haugeto
+ */
 public interface RequestInterceptor {
     /**
      * Invoked before a request is processed
